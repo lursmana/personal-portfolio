a99c516c53d04ab8f8043f7638360874de5d5996
lursmana <88488438+lursmana@users.noreply.github.com>
2023-02-16 15:39:46 +0400

    Added Cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import CV from "../../assets/my-cv.pdf"
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="" className="btn">
+      <a href={CV} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
